6dcdab4e3566cc42614b62388671a0899ceb2323
groob <vrancean+github@gmail.com>
2015-08-16 18:32:29 -0400

    release script

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERSION="0.0.2"
+
+echo "Building autopkgd version $VERSION"
+
+mkdir -p pkg
+
+build() {
+  echo -n "=> $1-$2: "
+  GOOS=$1 GOARCH=$2 go build -o pkg/autopkgd -ldflags "-X main.Version $VERSION"
+  du -h pkg/autopkgd
+}
+
+build "darwin" "amd64"
